c1beffea7b944404308b58d8fa9327b618a31cab
Chairunissa <203040048@mail.unpas.ac.id>
2022-10-10 14:20:40 +0700

    menyelesaikan video 6 view

---
+++ phpmvc/app/core/Constants.php
@@ -0,0 +1,5 @@
+<?php
+
+define('BASEURL', 'http://localhost/prakweb_a_203040048/phpmvc/public');
+define('DEFAULT_CONTROLLER', 'Home');
+define('DEFAULT_METHOD', 'index');
--- phpmvc/app/views/about/index.php
+++ phpmvc/app/views/about/index.php
@@ -1,5 +1,9 @@
 <html>
-<h1>About Me</h1>
+<div class="container">
+    <h1>About Me</h1>
+    <img src="<?= BASEURL; ?>/img/R.png" alt="Chairunissa" width="200" class="rounded-circle shadow">
 <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+</div>
+
 
 </html>
